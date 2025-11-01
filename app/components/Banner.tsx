f31fa200ce1f892a66cc49f9a74dd3e1b2cc96f5
zahra ellioun <zizi.ell75@gmail.com>
2025-11-01 12:14:59 +0330

    change text color in button in banner

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -13,11 +13,11 @@ const Banner = () => {
         className="h-auto w-full object-contain -scale-x-[-1] "
         priority
       />
-      <div className="absolute inset-0 flex flex-col justify-center px-10 md:px-20 lg:px-38 pt-10 lg:pt-18">
+      <div className="absolute inset-0 flex flex-col justify-center px-10 md:px-20 lg:px-38  pt-12 sm:pt-10 lg:pt-18">
         <p className="font-extrabold text-base sm:text-3xl md:text-4xl lg:text-5xl sm:mb-3">
           هرقسمت
         </p>
-        <p className="font-extrabold  text-base sm:text-3xl md:text-4xl lg:text-5xl text-[#C5A4FF] mb-2 sm:mb-6">
+        <p className="font-extrabold  text-base sm:text-3xl md:text-4xl lg:text-5xl text-[#C5A4FF] sm:mb-6">
           کمتر از یک دقیقه
         </p>
         <p className="hidden sm:block text-xl md:text-2xl lg:text-3xl font-light mb-2">
@@ -26,7 +26,7 @@ const Banner = () => {
         <p className="hidden sm:block text-xl md:text-2xl lg:text-3xl font-light mb-7">
           ادامه با تبلیغ یا کوین
         </p>
-        <div className="flex gap-3">
+        <div className="flex gap-3 text-white">
           <button className="text-xs sm:text-xl lg:text-2xl bg-[#8B5CF6] rounded-lg sm:rounded-xl px-3 py-2 lg:px-4 lg:py-3 flex justify-center items-center gap-2 shadow-2xl hover:bg-[#763dfa] duration-300 transition">
             <span>
               <VscTriangleRight />
